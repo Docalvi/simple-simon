4859aa8fbea2228ed9c54c2bcdb37547c1d6bc00
emilio alvarado <emiliolvrd@gmail.com>
2017-06-19 11:20:40 -0500

    Started controllers exercise added add,subtract, multiply,and divide methods to math controller

---
+++ IdeaProjects/spring-blog/src/main/java/com/codeup/controllers/PostsController.java
@@ -0,0 +1,11 @@
+package com.codeup.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by emilioalvarado on 6/19/17.
+ */
+@Controller
+public class PostsController {
+
+}
